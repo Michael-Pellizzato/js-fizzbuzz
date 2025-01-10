ed15d11d92dae9a1c106a2731523648cd63caae2
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-01-10 12:31:50 +0100

    correzione 1

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ for ( let i = 1; i <= 100; i++ ){
         console.log( "fizzbuzz")
     } else if ( i % 3 === 0 ){
         console.log ( "fizz" )
-    } else if ( 1 % 5 === 0 ){
+    } else if ( i % 5 === 0 ){
         console.log( "buzz" )
     } else {
         console.log ( i )
